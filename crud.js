ca840bd60bc6b564090a518a98cae682fa4218e8
Klosimierz <klosimierz@gmail.com>
2020-10-15 22:04:39 +0200

    Initial state

---
+++ crud.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const {Pool,Client} = require('pg');
+const {select_delegations,select_clients,insert_client,delete_client,edit_client} = require('./query_strings.json');
+const router = express.Router();
+const autocatch = require('./autocatch');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'practiceSql',
+  password: 'klosek79',
+  port: 5430,
+});
+
+router.get('/delegations', autocatch(async(req,res)=>{
+    const result = await pool.query(select_delegations);
+    res.status(200).send(result.rows);
+}));
+
+router.get('/getClients', autocatch(async(req,res)=>{
+    const result = await pool.query(select_clients);
+    res.status(200).send(result.rows);
+}));
+
+router.post('/insertClient', autocatch(async(req,res)=>{
+    const {NIP,REGON,NAZWA,VAT,ULICA,NRDOMU,NRMIESZKANIA} = req.body;
+    const result = await pool.query(insert_client,[NIP,REGON,NAZWA,VAT,ULICA,NRDOMU,NRMIESZKANIA]);
+    res.status(200).send(result);
+}));
+
+router.post('/deleteClient', autocatch(async(req,res)=>{
+    const {ID} = req.body;
+    const result = await pool.query(delete_client,[ID]);
+    res.status(200).send(result);
+}));
+
+router.post('/updateClient', autocatch(async(req,res)=>{
+    const {NIP,REGON,NAZWA,VAT,ULICA,NRDOMU,NRMIESZKANIA,ID} = req.body;
+    const result = await pool.query(edit_client,[NIP,REGON,NAZWA,VAT,ULICA,NRDOMU,NRMIESZKANIA,ID]);
+    res.status(200).send(result);
+}));
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const crud = require('./crud');
+
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept');
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    next();
+  });
+
+app.use(express.json());
+app.use('/api',crud);
+app.use(err=>{
+    console.log('Server error');
+})
+
+app.listen(4100,()=>{
+    console.log("Open");
+});
+
